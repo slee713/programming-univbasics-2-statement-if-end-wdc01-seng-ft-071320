8325726da0a38d8e3863c6a1cbb34b49221f8026
Steven Lee <steven.lee1393@gmail.com>
2020-06-29 13:16:56 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -16,7 +16,7 @@ puts "Code after if...end"
 # end
 # puts "Oh, and always wear sunscreen!"
 
-chance_of_rain = 0.0000001
+chance_of_rain = 0.200000001
 if chance_of_rain <= 0.25
   puts "Pack a sun shelter!"
 elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
